3a73c2e72c3d259319e8732969f83fa77317ef91
mavenrank <Maverickodes.mac@gmail.com>
2024-03-20 08:48:54 +0530

    Components and props

--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -2,8 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function Greeting() {
-    return <h4>Hello this is my first time using react</h4>;
+    return (
+        <>
+            <h4>Hello this is my first time using react</h4>
+            <p>Hello people</p>
+            <ul>
+                <li>List Element</li>
+            </ul>
+            <p>
+                <Message text="Good Morning! " />, <Person name="Anubhaw" />
+            </p>
+        </>
+    );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Person(usr) {
+    return (
+        <>
+            <span>{usr.name}</span>
+        </>
+    );
+}
+
+function Message(msg) {
+    return (
+        <>
+            <span>{msg.text}</span>
+        </>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Greeting />);
