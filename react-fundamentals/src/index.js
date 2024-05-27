ba684c2cb068e656319f947db63b8c53465c0d9f
mavenrank <Maverickodes.mac@gmail.com>
2024-05-27 20:22:25 +0530

    Used Props across Components

--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -1,24 +1,84 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+function Person(usr) {
+    return (
+        <>
+            <span>{usr.name}</span>
+        </>
+    );
+}
+
+function Message(msg) {
+    return (
+        <>
+            <span>{msg.text}</span>
+        </>
+    );
+}
+
 const user = {
     name: "Hedy Lamarr",
     imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
     imageSize: 90,
 };
 
+const Image = ({ source, alt }) => {
+    return (
+        <>
+            <img
+                src= {source}
+                alt = {alt}
+            />
+        </>
+    );
+}
+
+const Author = ({authorname}) => {
+    return (
+        <p>{authorname}</p>
+    )
+} 
+
+const Bookname = ({ bookname }) => {
+    return (
+        <p>{bookname}</p>
+    )
+}
+
+const Book = ({ bookname, source, alt, authorname }) => {
+    console.log(source)
+    return (
+        <>
+            <Bookname bookname={bookname} />
+            <img src={source} />
+            <Image src={source} alt={alt} />
+            <Author authorname={authorname} />
+        </>
+    )
+}
+
+const BookList = () => {
+    return (
+        <>
+            <Book
+                bookname="Atomic Habits"
+                source="https://m.media-amazon.com/images/I/71F4+7rk2eL._AC_UY327_FMwebp_QL65_.jpg"
+                alt="Image of book Atomic Habits"
+                authorname="James Clear"
+            />
+
+        </>
+    );
+}
+
+
 function Greeting() {
     return (
         <>
-            <h4>Hello this is my first time using react</h4>
-            <p>Hello people</p>
-            <ul>
-                <li>List Element</li>
-            </ul>
             <p>
-                <Message text="Good Morning " />, <Person name="Anubhaw" />
+                <Message text="Good Morning " />, <Person name="Roopam" />
             </p>
-
             <div style={{ display: "flex", flexDirection: "row", alignItems:"center"}}>
                 <p>{user.name}</p>
                 <img
@@ -36,21 +96,7 @@ function Greeting() {
     );
 }
 
-function Person(usr) {
-    return (
-        <>
-            <span>{usr.name}</span>
-        </>
-    );
-}
 
-function Message(msg) {
-    return (
-        <>
-            <span>{msg.text}</span>
-        </>
-    );
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Greeting />);
+root.render(<BookList/>);
