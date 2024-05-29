3bae962356a9c5044e80fecffce10b058ae01836
mavenrank <Maverickodes.mac@gmail.com>
2024-05-29 16:25:59 +0530

    Issue with DynamicBookList

--- Book Website - React Fundamentals/src/index.js
+++ Book Website - React Fundamentals/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css" 
+import "./index.css";
 
 function Person(usr) {
     return (
@@ -23,7 +23,6 @@ const user = {
     imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
     imageSize: 90,
 };
-
 
 function Greeting() {
     return (
@@ -54,41 +53,38 @@ function Greeting() {
     );
 }
 
-const Image = ({src, alt}) => {
+const Image = ({ src, alt }) => {
     return (
         <section>
             {console.log(src)}
             <img className="image" src={src} alt={alt} />
         </section>
     );
-}
+};
 
-const Author = ({authorname}) => {
-    return (
-        <p className="authorname">{authorname}</p>
-    )
-} 
+const Author = ({ authorname }) => {
+    return <p className="authorname">{authorname}</p>;
+};
 
 const Bookname = ({ bookname }) => {
-    return (
-        <p className="bookname">{bookname}</p>
-    )
-}
-
+    return <p className="bookname">{bookname}</p>;
+};
+let count = 0;
 const Book = ({ bookname, source, alt, authorname }) => {
+    count++;
+    console.log(count);
     return (
         <div className="outerborder col-large-3 col-med-6 col-small-12">
             <section className="book">
                 <Bookname bookname={bookname} />
-                {/* <img src={source} alt={alt} className="image" /> */}
                 <Image src={source} alt={alt} />
                 <Author authorname={authorname} />
             </section>
         </div>
     );
-}
+};
 
-const BookList = () => {
+const HarcodedBookList = () => {
     return (
         <section className="booklist">
             <Book
@@ -107,20 +103,92 @@ const BookList = () => {
                 bookname="The Lost Bookshop"
                 source="https://m.media-amazon.com/images/I/91Sy3S-198L._AC_UY327_FMwebp_QL65_.jpg"
                 alt="Image of book The Lost Bookshop"
-                authorname="Héctor García, Francesc Miralles"
+                authorname="Evie Woods"
             />
         </section>
     );
+};
+
+const bookNameList = ["Atomic Habits", "Ikigai", "The Lost Bookhouse"];
+const newBookName = bookNameList.map((newbookname) => {
+    return <p>{newbookname}</p>;
+});
+const imageSrcList = [
+    "https://m.media-amazon.com/images/I/71F4+7rk2eL._AC_UY327_FMwebp_QL65_.jpg",
+    "https://m.media-amazon.com/images/I/41jRRrqZipL._SY445_SX342_.jpg",
+    "https://m.media-amazon.com/images/I/91Sy3S-198L._AC_UY327_FMwebp_QL65_.jpg",
+];
+const newImageSrc = imageSrcList.map((newimagesrc) => {
+    return <p>{newimagesrc}</p>;
+});
+const imageAltList = [
+    "Image of Book Atomic Habits",
+    "Image of Book Ikigai",
+    "Image of Book The Lost Bookshop",
+];
+const newImageAlt = imageAltList.map((newimagealt) => {
+    return <p>{newimagealt}</p>;
+});
+const authorNameList = [
+    "James Clear",
+    "Héctor García, Francesc Miralles",
+    "Evie Woods",
+];
+const newAuthorName = authorNameList.map((newauthorname) => {
+    return <p>{newauthorname}</p>;
+});
+
+function DynamicBookList() {
+    let ctr = 1;
+    return (
+        <section className="booklist">
+            <Book
+                bookname={bookNameList[count]}
+                source={imageSrcList[count]}
+                alt={imageAltList[count]}
+                authorname={authorNameList[count]}
+            />
+            <Book
+                bookname={bookNameList[count + 1]}
+                source={imageSrcList[count + 1]}
+                alt={imageAltList[count + 1]}
+                authorname={authorNameList[count + 1]}
+            />
+            <Book
+                bookname={bookNameList[count + 2]}
+                source={imageSrcList[count + 2]}
+                alt={imageAltList[count + 2]}
+                authorname={authorNameList[count + 2]}
+            />
+        </section>
+    );
+}
+
+function DynamicBookList1() {
+    const booklist = bookNameList.map((item, index) => {
+        console.log(authorNameList[index]);
+        return (
+            <Book
+                bookname={item[index]}
+                source={imageSrcList[index]}
+                alt={imageAltList[index]}
+                authorname={authorNameList[index]}
+            />
+        );
+    });
+    console.log(booklist);
+    return <section className="booklist">{booklist}</section>;
 }
 
 const Webpage = () => {
     return (
         <div className="mainDiv">
             <h1>Welcome to our BookStore</h1>
-            <BookList></BookList>
+            {/* <HardcodedBookList></HardcodedBookList> */}
+            <DynamicBookList1></DynamicBookList1>
             <div className="emptyDiv"></div>
         </div>
-    )
-}
+    );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Webpage/>);
+root.render(<Webpage />);
