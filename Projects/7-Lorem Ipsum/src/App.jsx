8e549104f9eeb1c6f544ed9cd91baa595ee7ea7c
mav <Maverickodes.mac@gmail.com>
2024-06-23 11:46:08 +0530

    Fixed the Issue where state variable was being tampered with JSDOM function

--- Projects/7-Lorem Ipsum/src/App.jsx
+++ Projects/7-Lorem Ipsum/src/App.jsx
@@ -7,66 +7,76 @@ function App() {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(count);
+        console.log(`type of count is : ${typeof count}`);
 
         // ! we see that if we leave it to zero, then it is a number
         // ! if we change the value to any other number, then it is a String
-
+        //? issue couldn't recreate, fixed.
         let amount = parseInt(count);
         console.log(amount);
         console.log(typeof amount);
 
-        let article = document.getElementById("error-value");
-        console.log(article);
+        let errorText = document.getElementById("error-value");
+        console.log(errorText);
 
-        let loremtext = document.getElementById("lorem-text");
-        console.log(loremtext);
+        //???????? let loremText = document.getElementById("lorem-text");
+        //! DO NOT TAMPER WITH THE DOCUMENT ELEMENT SEPARATELY USING JSDOM,
+        //! THIS ELEMENT WAS BEING CONTORLLED BY TEXT STATE VARIABLE
+        //! USE ONLY SETTEXT TO CHANGE THE VALUES OF TEXT
 
         if (count < 0) {
-            article.innerText =
+            errorText.innerText =
                 "Woah there! How can you generate negative number of paragraphs? 🤔\nGive a positive value bucko!";
             return;
         } else if (count == 0) {
-            article.innerText =
+            errorText.innerText =
                 "Well you told me not to generate anything ¯\\_(ツ)_/¯\nGive me a positive value";
             return;
         } else if (count > 0 && count <= data.length) {
-            article.innerText = "";
-            setText(data.slice(0, amount));
+            errorText.innerText = "";
+            setText(data.slice(0, count));
             return;
         }
-        if (amount > data.length) {
-            if (amount > 50) {
+        if (count > data.length) {
+            if (count > 50) {
                 let userConfirm = window.confirm(
-                    `Are you Sure you wanna generate ${amount} paragraphs?`
+                    `Are you Sure you wanna generate ${count} paragraphs?`
                 );
                 //! issue present here, after user Declines and then accepts a larger value, the paras start from the previous value.
+                //? issue fixed,
+                //? the issue was where loremtext id html element was being tampered with.
+                //? when we had a text state variable dealing with it, we could have just setText([]), in userConfirm = false
                 if (userConfirm) {
-                    article.innerText = "";
+                    errorText.innerText = "";
                     let paragraphs = [];
-                    console.log(`Amount : ${amount}`);
+                    console.log(`Count : ${count}`);
                     console.log(`Paragraph Length : ${paragraphs.length} `);
-                    while (paragraphs.length < amount) {
+                    while (paragraphs.length < count) {
                         paragraphs = paragraphs.concat(data);
                     }
                     console.log(paragraphs.length);
-                    paragraphs = paragraphs.slice(0, amount);
+                    paragraphs = paragraphs.slice(0, count);
                     setText(paragraphs);
+                    setCount(0);
                     return;
                 } else {
-                    loremtext.innerText = "";
                     setCount(0);
-
-                    article.innerText = "Provide another value";
+                    //! loremText.innerText = "";
+                    //? THE ABOVE STATEMENT CHANGES IT FROM CONTROLLED COMP TO UNCONTROLLED COMP AND THEN BACK TO CONTROLLED COMP IN CASE THE USER CONFIRMS AGAIN
+                    //! THIS WILL BE TAKING CONTROL OF ELEMNT FROM REACT TO JS.
+                    //! DO NOT DO THIS WHEN A STATE VARIABLE IS ALREADY CONTROLLING THE ELEMENT
+                    setText([]); //? THIS IS A BETTER WAY OF SETTING THE VALUE, USING REACT ITSELF.
+                    errorText.innerText = "Provide another value";
                     return;
                 }
             } else {
-                article.innerText = "";
+                errorText.innerText = "";
                 let paragraphs = [];
-                while (paragraphs.length < amount) {
+                while (paragraphs.length < count) {
                     paragraphs = paragraphs.concat(data);
                 }
                 console.log(paragraphs.length);
-                paragraphs = paragraphs.slice(0, amount);
+                paragraphs = paragraphs.slice(0, count);
                 setText(paragraphs);
                 return;
             }
